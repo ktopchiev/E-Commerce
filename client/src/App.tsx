db66eeaa389fa792623e16ad3668d6a46b7d8031
Karol Topchiev <karlostopchiev@gmail.com>
2024-08-07 14:17:15 +0300

    Simple fetch data to test API and adding CORS config to the API

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,25 @@
+import { useState, useEffect } from "react";
+
 function App() {
 
-  return (
-    <div>
-      <h1>E-Commerce</h1>
-    </div>
-  )
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		fetch('http://localhost:5000/api/products')
+			.then(response => response.json())
+			.then(data => setProducts(data));
+	}, [])
+
+	return (
+		<div>
+			<h1 style={{ color: 'red' }}>E-Commerce</h1>
+			<ul>
+				{products.map((product) =>
+					(<li key={product.id}>{product.name}</li>)
+				)}
+			</ul>
+		</div>
+	)
 }
 
 export default App
