3b1b111128973c641da77f0b07090ab418301af0
Karol Topchiev <karlostopchiev@gmail.com>
2024-08-15 17:09:27 +0300

    added react-toastify

--- client/src/App/api/agent.ts
+++ client/src/App/api/agent.ts
@@ -1,8 +1,29 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { toast } from "react-toastify";
 
 axios.defaults.baseURL = 'http://localhost:5000/api/';
 
 const responseBody = (response: AxiosResponse) => (response.data);
+
+axios.interceptors.response.use(async response => {
+    return response
+}, (error: AxiosError) => {
+    const { data, status } = error.response as AxiosResponse;
+    switch (status) {
+        case 400:
+            toast.error(data.title);
+            break;
+        case 401:
+            toast.error(data.title);
+            break;
+        case 500:
+            toast.error(data.title);
+            break;
+        default:
+            break;
+    }
+    return Promise.reject(error.response)
+})
 
 const requests = {
     get: (url: string) => axios.get(url).then(responseBody),
@@ -16,8 +37,17 @@ const Catalog = {
     details: (id: number) => requests.get(`products/${id}`)
 }
 
+const TestErrors = {
+    get400Error: () => requests.get('buggy/bad-request'),
+    get401Error: () => requests.get('buggy/unauthorized'),
+    get404Error: () => requests.get('buggy/not-found'),
+    get500Error: () => requests.get('buggy/server-error'),
+    getValidationError: () => requests.get('buggy/validation-error')
+}
+
 const agent = {
     Catalog,
+    TestErrors
 }
 
 export default agent
