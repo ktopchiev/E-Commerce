4bf15dab028f61aa31b3ed915ae7e1dc1dfdd54f
Karol Topchiev <karlostopchiev@gmail.com>
2024-10-23 19:35:01 +0300

    Added mui pagination

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -3,7 +3,10 @@ import { useEffect } from "react";
 import LoadingComponent from "../../App/layout/LoadingComponent";
 import { useAppDispatch, useAppSelector } from "../../App/store/configureStore";
 import { fetchFiltersAsync, fetchProductsAsync, productsSelectors } from "./catalogSlice";
-import { Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Grid2, Paper, Radio, RadioGroup, TextField } from "@mui/material";
+import {
+    Box, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Grid2,
+    Pagination, Paper, Radio, RadioGroup, TextField, Typography
+} from "@mui/material";
 
 function Catalog() {
 
@@ -45,7 +48,7 @@ function Catalog() {
                                 name="radio-buttons-group"
                             >
                                 {sortOptions.map(({ value, label }) =>
-                                    <FormControlLabel value={value} control={<Radio />} label={label} />
+                                    <FormControlLabel value={value} control={<Radio />} label={label} key={value} />
                                 )}
 
                             </RadioGroup>
@@ -56,7 +59,7 @@ function Catalog() {
                             <FormLabel>Brands</FormLabel>
                             <FormGroup>
                                 {brands.map(brand =>
-                                    <FormControlLabel control={<Checkbox />} label={brand} />
+                                    <FormControlLabel control={<Checkbox />} label={brand} key={brand} />
                                 )}
                             </FormGroup>
                         </FormControl>
@@ -66,7 +69,7 @@ function Catalog() {
                             <FormLabel>Types</FormLabel>
                             <FormGroup>
                                 {types.map(type =>
-                                    <FormControlLabel control={<Checkbox />} label={type} />
+                                    <FormControlLabel control={<Checkbox />} label={type} key={type} />
                                 )}
                             </FormGroup>
                         </FormControl>
@@ -74,6 +77,14 @@ function Catalog() {
                 </Grid2>
                 <Grid2 size={9}>
                     <ProductList products={products} />
+                </Grid2>
+                <Grid2 size={3}>
+                    <Typography>Showing page 1 of 3</Typography>
+                </Grid2>
+                <Grid2 size={9} display={'flex'} justifyContent={'center'} alignItems={'center'}>
+                    <Box>
+                        <Pagination count={10} />
+                    </Box>
                 </Grid2>
             </Grid2>
         </>
