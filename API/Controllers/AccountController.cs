0266ecaf6ed03ed4f13ee6a40a50ea8a8bc2e7d6
ktopchiev <karlostopchiev@gmail.com>
2024-12-12 13:28:53 +0200

    .

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+
     public class AccountController : BaseApiController
     {
         private readonly UserManager<User> _userManager;
