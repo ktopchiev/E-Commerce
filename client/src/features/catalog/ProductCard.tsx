9329d77c83b0896893681b6a02cf4b060c5eba09
ktopchiev <karlostopchiev@gmail.com>
2025-02-16 20:32:18 +0200

    Added nav functionality to product card

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,6 +1,6 @@
-import { Typography, Button, Card, CardActions, CardContent, CardMedia, Avatar, CardHeader } from "@mui/material"
+import { Typography, Button, Card, CardActions, CardContent, CardMedia, Avatar, CardHeader, CardActionArea } from "@mui/material"
 import { Product } from "../../App/models/product"
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { LoadingButton } from '@mui/lab';
 import { AddShoppingCart, VisibilityOutlined } from "@mui/icons-material";
 import { useAppDispatch, useAppSelector } from "../../App/store/configureStore";
@@ -14,37 +14,40 @@ function ProductCard({ product }: ProductCardProps) {
 
     const { status } = useAppSelector(state => state.basket)
     const dispatch = useAppDispatch();
+    const navigate = useNavigate();
 
     return (
         <Card sx={{ maxWidth: 345 }} key={product.id}>
-            <CardHeader
-                avatar={
-                    <Avatar variant="circular" sx={{ bgcolor: 'secondary.main' }}>
-                        {product.name.toString().charAt(0).toUpperCase()}
-                    </Avatar>
-                }
-                title={product.name}
-                titleTypographyProps={{
-                    sx: {
-                        fontWeight: 'bold',
-                        color: 'primary.main'
+            <CardActionArea onClick={() => navigate(`/catalog/${product.id}`)}>
+                <CardHeader
+                    avatar={
+                        <Avatar variant="circular" sx={{ bgcolor: 'secondary.main' }}>
+                            {product.name.toString().charAt(0).toUpperCase()}
+                        </Avatar>
                     }
-                }}
-            >
-            </CardHeader>
-            <CardMedia
-                sx={{ height: 140, backgroundSize: 'contain', bgcolor: 'primary.light' }}
-                image={product.pictureUrl}
-                title={product.name}
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h6" sx={{ color: 'secondary.main' }}>
-                    ${(product.price / 100).toFixed(2)}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    {product.type}/{product.brand}
-                </Typography>
-            </CardContent>
+                    title={product.name}
+                    titleTypographyProps={{
+                        sx: {
+                            fontWeight: 'bold',
+                            color: 'primary.main'
+                        }
+                    }}
+                >
+                </CardHeader>
+                <CardMedia
+                    sx={{ height: 140, backgroundSize: 'contain', bgcolor: 'primary.light' }}
+                    image={product.pictureUrl}
+                    title={product.name}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h6" sx={{ color: 'secondary.main' }}>
+                        ${(product.price / 100).toFixed(2)}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {product.type}/{product.brand}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
             <CardActions>
                 <LoadingButton
                     loading={status === 'pendingAddItem' + product.id}
@@ -61,6 +64,7 @@ function ProductCard({ product }: ProductCardProps) {
                     <VisibilityOutlined />
                 </Button>
             </CardActions>
+
         </Card >
     )
 }
