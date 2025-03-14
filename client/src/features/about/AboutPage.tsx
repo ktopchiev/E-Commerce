9a6e1b2ed4653eec695fcd332f3c0e290fedd1b3
ktopchiev <karlostopchiev@gmail.com>
2025-03-14 17:08:31 +0200

    test

--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -1,6 +1,8 @@
 function AboutPage() {
     return (
-        <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem omnis unde possimus, sed nemo amet minima officiis deleniti recusandae quos nisi labore dolores deserunt eum quas et explicabo est rem.</div>
+        <div>
+            AboutPage
+        </div>
     )
 }
 
