949632f5528c7fad403feea9d13e35d57a1e589e
Karol Topchiev <karoltopchiev@outlook.com>
2025-11-11 20:10:08 +0200

    added contact page

--- client/src/features/contact/ContactPage.tsx
+++ client/src/features/contact/ContactPage.tsx
@@ -1,11 +1,9 @@
-import { Typography } from "@mui/material";
+import { Box, Container, IconButton, Typography } from "@mui/material";
 
 export default function ContactPage() {
 
 
     return (
-        <>
-            <Typography variant="h6" sx={{ marginBottom: '20px' }}>Contact Page 123</Typography>
-        </>
+        
     )
 }
